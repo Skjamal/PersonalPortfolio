e18a4d96bdc44ea24da1a2cd6d97ca6e05b15bd8
Skjamal <satinder.jamal@gmail.com>
2019-01-08 22:00:09 -0700

    initial commit

---
+++ portfolio/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\satinderjamal\desktop\practicepython\personalportfoliolyndaproject\portfolio\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
